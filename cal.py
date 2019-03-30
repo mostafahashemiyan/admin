9485a3474f6926c1b3529d2fca8fd3c9d1d30b99
mostafahashemiyan <49098593+mostafahashemiyan@users.noreply.github.com>
2019-03-30 20:37:16 +0430

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,32 @@
+print("welcome to my calculator")
+print( "option")
+print ("type add for add 2numbers")
+print ("type subtract for subtract 2numbers")
+print ("type muliply for multiply 2numbers")
+print ("type divide for divide 2numbers")
+print ("type quite for exit ")
+
+user_input = input (":")
+if user_input == "quite"
+print("exit")
+elif user_input == "add"
+num1= float (input(enter number one))
+num2=float (input(enter nummer two))
+result= str (num1 + num2)
+print ("your result is "+result)
+elif user_input == "subtract"
+num1= float (input(enter number one))
+num2=float (input(enter nummer two))
+result= str(num1 - num 2)
+print ("your result is "+result)
+elif user_input == "multiply"
+num1= float (input(enter number one))
+num2=float (input(enter nummer two))
+result= str(num 1 * num 2)
+print ("your result is "+result)
+elif user_input == "divide"
+num1= float (input(enter number one))
+num2=float (input(enter nummer two))
+result= srt (num 1 / num 2)
+print ("your result is "+result)
+else = print (" Error ")
